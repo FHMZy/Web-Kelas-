39adfa3e942fc1a0776b6c164b8cb9abcfb3542a
FHMZy <157804614+FHMZy@users.noreply.github.com>
2024-01-29 19:52:24 +0700

    Add files via upload

---
+++ src/components/Mapel/Selasa.jsx
@@ -0,0 +1,41 @@
+const Selasa = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">PBO</div>
+					<div className="flex justify-center items-center text-sm">07.15-08.35</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">PEMROGAMAN WEB</div>
+					<div className="flex justify-center items-center text-sm">08.35-09.55</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">09.55-10.15</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">PEMROGAMAN WEB</div>
+					<div className="flex justify-center items-center text-sm">10.15-12.15</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">12.15-12.45</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">PEMROGAMAN WEB</div>
+					<div className="flex justify-center items-center text-sm">12.45-13.25</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">BP/BK</div>
+					<div className="flex justify-center items-center text-sm">13.25-14.05</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">PBO</div>
+					<div className="flex justify-center items-center text-sm">14.05-15.25</div>
+				</div>
+        </>
+    )
+} 
+
+export default Selasa;
